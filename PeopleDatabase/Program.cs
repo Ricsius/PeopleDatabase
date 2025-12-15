8195eb0ccfb055b8d2e9deb07be3fd6ccd6b7e01
Ricsius <hajnal.ricsi@gmail.com>
2025-12-15 18:34:57 +0100

    Migrated service setup to an extension method

--- PeopleDatabase/Program.cs
+++ PeopleDatabase/Program.cs
@@ -1,11 +1,5 @@
-using ServiceContracts;
-using Services;
-using Entities;
-using Microsoft.EntityFrameworkCore;
-using RepositoryContracts;
-using Repositories;
 using Serilog;
-using PeopleDatabase.Filters.ActionFilters;
+using PeopleDatabase.StartupExtensions;
 
 namespace PeopleDatabase
 {
@@ -30,29 +24,7 @@ namespace PeopleDatabase
                 .ReadFrom.Configuration(context.Configuration)
                 .ReadFrom.Services(serviceProvider);
             });
-            builder.Services.AddTransient<ResponseHeaderActionFilter>();
-            builder.Services.AddControllersWithViews(options => 
-            {
-                var factory = new ResponseHeaderActionFilterFactory("MyKey_FromGlobal", "MyValue_FromGlobal", 2);
-                options.Filters.Add(factory.CreateInstance(builder.Services.BuildServiceProvider()));
-            });
-            builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
-            builder.Services.AddScoped<IPeopleRepository, PeopleRepository>();
-            builder.Services.AddScoped<ICountriesService, CountriesService>();
-            builder.Services.AddScoped<IPeopleService, PeopleService>();
-            builder.Services.AddHttpLogging(options => 
-            {
-                options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties
-                | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
-            });
-
-            if (!builder.Environment.IsEnvironment("Test"))
-            {
-                builder.Services.AddDbContext<PeopleDbContext>(options =>
-                {
-                    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-                });
-            }
+            builder.Services.ConfigureServices(builder.Configuration, builder.Environment);
             
             var app = builder.Build();
 
---
+++ PeopleDatabase/StartupExtensions/ConfigureServicesExtension.cs
@@ -0,0 +1,40 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using PeopleDatabase.Filters.ActionFilters;
+using Repositories;
+using RepositoryContracts;
+using ServiceContracts;
+using Services;
+
+namespace PeopleDatabase.StartupExtensions
+{
+    public static class ConfigureServicesExtension
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            services.AddTransient<ResponseHeaderActionFilter>();
+            services.AddControllersWithViews(options =>
+            {
+                var factory = new ResponseHeaderActionFilterFactory("MyKey_FromGlobal", "MyValue_FromGlobal", 2);
+                options.Filters.Add(factory.CreateInstance(services.BuildServiceProvider()));
+            });
+            services.AddScoped<ICountriesRepository, CountriesRepository>();
+            services.AddScoped<IPeopleRepository, PeopleRepository>();
+            services.AddScoped<ICountriesService, CountriesService>();
+            services.AddScoped<IPeopleService, PeopleService>();
+            services.AddHttpLogging(options =>
+            {
+                options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties
+                | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
+            });
+
+            if (!environment.IsEnvironment("Test"))
+            {
+                services.AddDbContext<PeopleDbContext>(options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("Default"));
+                });
+            }
+        }
+    }
+}
